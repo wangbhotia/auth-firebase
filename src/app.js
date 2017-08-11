0c8a0edabf9ea135f73f80e843a80e338ffcc6a7
Wang Bhotia <wangbhotia@gmail.com>
2017-08-11 16:23:15 -0600

    change

--- src/app.js
+++ src/app.js
@@ -6,14 +6,7 @@ import LoginForm from './components/LoginForm';
 
 class App extends Component {
 	componentWillMount() {
-		firebase.initializeApp({
-	    apiKey: 'AIzaSyBCh5mgzTrV8ePE9wjVJgQxnW8G5edFJzo',
-	    authDomain: 'auth-afd42.firebaseapp.com',
-	    databaseURL: 'https://auth-afd42.firebaseio.com',
-	    projectId: 'auth-afd42',
-	    storageBucket: 'auth-afd42.appspot.com',
-	    messagingSenderId: '215404307083'
-	  });
+		
 	}
 
 	render() {
